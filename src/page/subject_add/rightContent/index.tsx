a2d72f170fd9fb8ce59e57398aa76d07eae4e756
masterder <1813708165@qq.com>
2023-11-19 22:03:41 +0800

    1119

--- src/page/subject_add/rightContent/index.tsx
+++ src/page/subject_add/rightContent/index.tsx
@@ -1,25 +1,36 @@
-
 import { useRef } from "react";
 import styles from "./index.module.css";
 import Upload from "./upload";
 import { Form, Input, Button } from "antd";
 import TextArea from "antd/es/input/TextArea";
+import { upload_imgs } from "@/util";
+import { useSelector } from "react-redux";
+import { select_active_two } from "@/store/slice/subject";
+import axios from "axios";
 export default function RightContent() {
   const [form] = Form.useForm();
-  let activeFile:any = useRef([])
+  let activeFile: any = useRef([]);
+  const two_obj: any = useSelector(select_active_two);
+
+
+
   function fileChange(info: any) {
-    console.log(info)
-    activeFile.current = info
+    console.log(info);
+    activeFile.current = info;
   }
-  async function submitClick() {   //图片上传
-const formData = await form.validateFields()
-if(formData){
-  console.log(formData)
-  if(activeFile.current?.fileList?.length){
-  }
+  async function submitClick() {
+    //图片上传
+    const formData = await form.validateFields();
+    if (formData) {
+      console.log(formData);
+      if (activeFile.current?.fileList?.length) {
+        const imgs_url_arr = await upload_imgs(activeFile);
+        formData.img = imgs_url_arr;
+      }
+      formData.two_id = two_obj.value;
 
-
-}
+    }
+    await axios.post("/api/topic",formData)
   }
   return (
     <Form
@@ -45,7 +56,9 @@ if(formData){
         <TextArea />
       </Form.Item>
       <Upload change={fileChange} />
-      <Button danger onClick={submitClick}>保存</Button>
+      <Button danger onClick={submitClick}>
+        保存
+      </Button>
     </Form>
   );
 }
